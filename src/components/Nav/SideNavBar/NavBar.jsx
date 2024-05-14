d82f0637fa85dbd2d6e30d229cfe0948ce039044
Conrado julio <conradojuliosisnero@gmail.com>
2024-05-14 18:47:32 -0500

    error de hidrataciiion de componentes

--- src/components/Nav/SideNavBar/NavBar.jsx
+++ src/components/Nav/SideNavBar/NavBar.jsx
@@ -2,13 +2,11 @@ import "./navbar.css";
 import "@/app/styles/styles.css";
 import Nav from "../Nav";
 import LibrarySideBar from "./library/librarySideBar";
-import ExtraOptions from "../Extra/ExtraOptions";
-import ContaineerLinks from "../ContainerLinks/ContaineerLinks";
 
 export default function NavBar() {
   return (
     <nav class="navbar">
-      {/* options  */}
+      {/* options nav */}
       <Nav />
       {/* library  */}
       <LibrarySideBar />
