35af7890978af5d0a896f2bd669b830b463560d6
Conrado julio <conradojuliosisnero@gmail.com>
2024-06-14 11:19:33 -0500

    organizando los layouts para el llamado a los endpoints

---
+++ src/app/playlist/page.jsx
@@ -0,0 +1,5 @@
+import Playlist from '@/components/Playlist/Playlist'
+
+export default function page() {
+  return <Playlist/>;
+}
---
+++ src/components/Artist/Artist.jsx
@@ -0,0 +1,14 @@
+import Container from "../Contained/Contained";
+import MusicCard from "../Home/CardSearch/Card";
+
+const Artist = () => {
+  return (
+    <Container name="Artist">
+      <MusicCard />
+      <MusicCard />
+      <MusicCard />
+    </Container>
+  );
+};
+
+export default Artist;
