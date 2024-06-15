a8e2bdbac5a360a7724ca90949ddc1376f3e8b59
Conrado julio <conradojuliosisnero@gmail.com>
2024-06-15 15:20:12 -0500

    fetch de las canciones de spotify

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,12 +1,17 @@
 import Container from "@/components/Contained/Contained";
 import MusicCard from "./CardSearch/Card";
+import getSongs from "@/services/Spotify/songs";
 
-export default function Home() {
+export default async function Home() {
+  const {tracks} = await getSongs();
+
+  console.log(tracks[0].album)
+
   return (
     <Container name="Home">
-      <MusicCard />
-      <MusicCard />
-      <MusicCard />
+      {SONGS?.tracks?.map((track, index) => (
+        <MusicCard track={track} key={index} />
+      ))}
     </Container>
   );
 }
