1f801b951c50665609375c172fede12349ccc0e5
Conrado julio <conradojuliosisnero@gmail.com>
2024-08-14 08:59:06 -0500

    preparacion del formulario de inicio de sesion providers y nextauth

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,51 +1,195 @@
+"use client";
+import { CloseEyeSvg, EyeSvg } from "@/data/svg";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
 
 export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordValidate, setValidatedPassword] = useState(null);
+  const [nameValidate, setValidatedName] = useState(null);
+  const [showPassword, setShowPassword] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(null);
+  // router
+  const router = useRouter();
+
+  // regular expression to validate password
+  const validatePassword = (password) => {
+    /* esta expresion regular solo acepta mayusculas, minusculas, numeros y simbolos
+      valida que el password tenga al menos una mayuscula, una minuscula, un numero y un simbolo */
+    const regex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    const isValid = regex.test(password);
+    if (!isValid) {
+      setValidatedPassword(false);
+      setIsError(true);
+    } else {
+      setValidatedPassword(true);
+      setIsError(false);
+    }
+  };
+
+  // mostra o oculta la password
+  const showPasswordHandler = () => {
+    setShowPassword(!showPassword);
+  };
+
+  // validate username
+  const validateUserName = (username) => {
+    if (username !== "") {
+      /* esta expresion regular solo acepta letras y numeros
+      con un maximo de 20 caracteres*/
+      const regex = /^[a-z\sA-Z0-9]{1,20}$/i;
+      const isValid = regex.test(username);
+      if (!isValid) {
+        setValidatedName(false);
+      } else {
+        setValidatedName(true);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (password !== "") {
+      validatePassword(password);
+    }
+  }, [password]);
+
+  useEffect(() => {
+    if (username !== "") {
+      validateUserName(username);
+    }
+  }, [username]);
+
+  // submit data
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    if (username === "" && password === "") {
+      setIsError("Rellene los campos para iniciar sesión.");
+      return;
+    }
+    setIsLoading(true);
+    try {
+      const response = await fetch("/api/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      });
+      const data = await response.json();
+      if (data.status === "ok") {
+        router.push("/");
+      } else {
+        setIsError("ocurrio un error al iniciar sesión.");
+      }
+    } catch (error) {
+      setIsError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
     <div className="flex items-center justify-center h-screen bg-[#121212] text-white">
       <div className="w-full max-w-md p-6 bg-[#1a1a1a] rounded-lg shadow-lg">
+        {/* title */}
         <div className="mb-6 text-center">
           <h1 className="text-3xl font-bold">Iniciar sesión</h1>
           <p className="text-muted-foreground">
-            Ingresa tus credenciales para acceder a Spotify.
+            Ingresa tus credenciales para acceder a{" "}
+            <Link
+              href="/about"
+              style={{ color: "#1db954", textDecoration: "underline" }}
+            >
+              Spotify Clone.
+            </Link>
           </p>
         </div>
-        <form className="space-y-4">
+        {/* form */}
+        <form className="space-y-4 relative" onSubmit={handleSubmit}>
           <div>
             <label
               htmlFor="username"
               className="block mb-2 text-sm font-medium text-muted-foreground"
             >
-              Nombre de usuario
+              Nombre de usuario *
             </label>
             <input
               id="username"
+              name="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
               type="text"
               placeholder="Ingresa tu nombre de usuario"
-              className="w-full px-3 py-2 bg-[#2a2a2a] border-none rounded-md focus:outline-none focus:ring-2 focus:ring-[#1db954]"
+              className={` ${
+                nameValidate
+                  ? "focus:outline-none focus:ring-2 focus:ring-[#1db954]"
+                  : "focus:outline-none focus:ring-2 focus:ring-[#f15252]"
+              } w-full px-3 py-2 bg-[#2a2a2a] border-none rounded-md`}
             />
+            {username !== "" && !nameValidate && (
+              <p className="text-[#f15252] text-sm">
+                el nombre no debe de tener caracteres especiales y un maximo de
+                20 caracteres
+              </p>
+            )}
           </div>
-          <div>
+
+          <div className="relative">
             <label
               htmlFor="password"
               className="block mb-2 text-sm font-medium text-muted-foreground"
             >
-              Contraseña
+              Contraseña *
             </label>
             <input
               id="password"
-              type="password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              type={showPassword ? "text" : "password"}
               placeholder="Ingresa tu contraseña"
-              className="w-full px-3 py-2 bg-[#2a2a2a] border-none rounded-md focus:outline-none focus:ring-2 focus:ring-[#1db954]"
+              className={`${
+                passwordValidate
+                  ? "focus:outline-none focus:ring-2 focus:ring-[#1db954]"
+                  : "focus:outline-none focus:ring-2 focus:ring-[#f15252]"
+              } w-full px-3 py-2 bg-[#2a2a2a] border-none rounded-md`}
             />
+            {/* Icons for show/hide password */}
+            <div
+              className="absolute right-2 top-8 cursor-pointer"
+              onClick={showPasswordHandler}
+            >
+              {!showPassword ? (
+                <EyeSvg width={28} height={28} />
+              ) : (
+                <CloseEyeSvg width={28} height={28} />
+              )}
+            </div>
+            {password !== "" && !passwordValidate && (
+              <p className="text-[#f15252] text-[12px]">
+                La contraseña debe de tener al menos 8 caracteres una mayuscula,
+                una minuscula, un numero y un simbolo
+              </p>
+            )}
           </div>
+
+          {isError && <p className="text-[#f15252] text-[12px]">{isError}</p>}
+
           <button
             type="submit"
             className="w-full px-4 py-2 font-medium text-white bg-[#1db954] rounded-md hover:bg-[#1ed760] focus:outline-none focus:ring-2 focus:ring-[#1db954]"
+            onClick={() => ""}
           >
-            Iniciar sesión
+            {isLoading ? "Iniciando Sesión..." : "Iniciar sesión"}
           </button>
         </form>
+
+        {/* Footer */}
         <div className="mt-6 text-center">
           <Link
             href="/forgot-password"
@@ -54,7 +198,10 @@ export default function Login() {
             Olvidaste la contraseña?
           </Link>
           <span className="mx-2 text-muted-foreground">·</span>
-          <Link href="/register" className="text-sm text-muted-foreground hover:underline">
+          <Link
+            href="/register"
+            className="text-sm text-muted-foreground hover:underline"
+          >
             Crear una cuenta
           </Link>
         </div>
---
+++ src/data/svg.js
@@ -0,0 +1,38 @@
+export const CloseEyeSvg = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlSpace="preserve"
+    width={800}
+    height={800}
+    fill="#fff"
+    style={{
+      fillRule: "evenodd",
+      clipRule: "evenodd",
+      strokeLinejoin: "round",
+      strokeMiterlimit: 2,
+    }}
+    viewBox="0 0 32 32"
+    {...props}
+  >
+    <path d="M5.112 18.784 2.959 20.94a1.5 1.5 0 0 0 2.122 2.12l2.666-2.668c1.898.983 4.19 1.806 6.773 2.041V26a1.5 1.5 0 0 0 3 0v-3.571c2.147-.201 4.091-.806 5.774-1.571l3.199 3.202a1.5 1.5 0 0 0 2.122-2.12l-2.579-2.581c2.59-1.665 4.091-3.369 4.091-3.369a1.501 1.501 0 0 0-2.254-1.98S23.059 19.5 16 19.5c-7.059 0-11.873-5.49-11.873-5.49a1.501 1.501 0 0 0-2.254 1.98s1.175 1.334 3.239 2.794Z" />
+  </svg>
+)
+
+export const EyeSvg = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={800}
+    height={800}
+    fill="#fff"
+    viewBox="0 0 48 48"
+    {...props}
+  >
+    <title>{"eye-close-solid"}</title>
+    <g data-name="Layer 2">
+      <path fill="none" d="M0 0h48v48H0z" data-name="invisible box" />
+      <g data-name="icons Q2">
+        <path d="M45.3 22.1C43.2 19.5 35.4 11 24 11a23.4 23.4 0 0 0-3.8.3l19.7 19.8a30.1 30.1 0 0 0 5.4-5.2 3 3 0 0 0 0-3.8ZM41.1 38.3 29.4 26.6A5.9 5.9 0 0 1 24 30a6 6 0 0 1-6-6 5.9 5.9 0 0 1 3.4-5.4L9.7 6.9a2 2 0 0 0-2.8 2.8l4.8 4.8a31.4 31.4 0 0 0-9 7.6 3 3 0 0 0 0 3.8C4.8 28.5 12.6 37 24 37a25.2 25.2 0 0 0 8.5-1.6l5.8 5.7a2 2 0 1 0 2.8-2.8Z" />
+      </g>
+    </g>
+  </svg>
+);
